b92c69d46fffee63054badf07965d9b6cb53034b
developerTajul <developer.tajul@gmail.com>
2022-01-21 15:47:13 +0600

    fixed

--- index.php
+++ index.php
@@ -6,9 +6,9 @@ require_once('functions.php');
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
+		<title>3rd class</title>
 	</head>
 	<body>
-		<h1>hello Bondura</h1>
+		<h1>This is 3rd class</h1>
 	</body>
 </html>
