4ab106f4e3bde54fbfb523bf3514f6ae46998832
developerTajul <developer.tajul@gmail.com>
2022-01-21 15:17:03 +0600

    fixed

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('functions.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+	<body>
+		<h1>hello Bondura</h1>
+	</body>
+</html>
